a56b34d6c112d59175acb4d7b1adbaf66e8c34bf
Yiran Jing <j1burn@outlook.com>
2024-04-16 22:37:20 +0100

    projectile refactor

--- Assets/Scripts/Controllers/ProjectileController.cs
+++ Assets/Scripts/Controllers/ProjectileController.cs
@@ -1,3 +1,4 @@
+using System;
 using Entities;
 using UnityEngine;
 
@@ -32,6 +33,52 @@ namespace Controllers
             _pe = GetComponent<ProjectileEntity>();
             gameObject.transform.localScale *= _pe.Scale;
             ChangeDirection(shootDirection);
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (_pe.damageOverTime) return;
+            if (owner.CompareTag("Player") && other.gameObject.CompareTag("AliveEnemy"))
+            {
+                EnemyController enemy = other.gameObject.GetComponent<EnemyController>();
+                if (enemy is not null)
+                {
+                    if(_pe.Hit(enemy.Entity))
+                        enemy.GetHurt();
+                }
+            } else if (owner.CompareTag("AliveEnemy") && other.gameObject.CompareTag("Player"))
+            {
+                PlayerController player = other.gameObject.GetComponent<PlayerController>();
+                if (player is not null)
+                {
+                    if (_pe.Hit(player.Entity))
+                        player.GetHurt();
+                }
+            }
+            else return;
+            Destroy(gameObject);
+        }
+
+        private void OnCollisionStay2D(Collision2D other)
+        {
+            if (!_pe.damageOverTime) return;
+            if (owner.CompareTag("Player") && other.gameObject.CompareTag("AliveEnemy"))
+            {
+                EnemyController enemy = other.gameObject.GetComponent<EnemyController>();
+                if (enemy is not null)
+                {
+                    if(_pe.Hit(enemy.Entity))
+                        enemy.GetHurt();
+                }
+            } else if (owner.CompareTag("AliveEnemy") && other.gameObject.CompareTag("Player"))
+            {
+                PlayerController player = other.gameObject.GetComponent<PlayerController>();
+                if (player is not null)
+                {
+                    if (_pe.Hit(player.Entity))
+                        player.GetHurt();
+                }
+            }
         }
 
         public void ChangeDirection(Vector2 direction)
